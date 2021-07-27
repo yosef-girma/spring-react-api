1d38c3b271813119b589b91115a432f5e1315fa0
Yosef Girma <zanonymouscity@gmail.com>
2021-07-27 12:55:22 +0300

    Cross orgin added

--- src/main/java/com/example/springreactcrud/controllers/EmployeeController.java
+++ src/main/java/com/example/springreactcrud/controllers/EmployeeController.java
@@ -3,12 +3,14 @@ package com.example.springreactcrud.controllers;
 import com.example.springreactcrud.models.Employee;
 import com.example.springreactcrud.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
