838cfd414569c09119ab471e166a5c7d94957818
Yosef Girma <zanonymouscity@gmail.com>
2021-07-27 14:47:54 +0300

    add get employee by id

--- src/main/java/com/example/springreactcrud/controllers/EmployeeController.java
+++ src/main/java/com/example/springreactcrud/controllers/EmployeeController.java
@@ -1,12 +1,11 @@
 package com.example.springreactcrud.controllers;
 
+import com.example.springreactcrud.exceptions.ResourceNotFoundException;
 import com.example.springreactcrud.models.Employee;
 import com.example.springreactcrud.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +21,23 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployee() {
         return employeeRepository.findAll();
+    }
+
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    // get employe by id
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id) {
+        Employee emp = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(
+                "Employee not exist with id :" + id
+        ));
+
+        return ResponseEntity.ok(emp);
     }
 
 }
